778830506f0461dedcd63f216d238c70d17a9e0a
shin seung jae <shinseungjae1234@gmail.com>
2022-05-12 01:31:26 +0900

    Initial Commit

---
+++ ramenTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ramenTimer
+//
+//  Created by 신승재 on 2022/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
